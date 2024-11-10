b50a482484e36baf3096355a5ac5051d98f22e86
Simona Tingleff Kardel <simonatingleffkardel@Simonas-MacBook-Pro.local>
2024-11-10 11:03:46 +0100

    modify ensemble schema

--- apps/api/src/ensemble/schema/ensemble.schema.ts
+++ apps/api/src/ensemble/schema/ensemble.schema.ts
@@ -3,21 +3,30 @@ import { Document, Types } from 'mongoose';
 import { MusicianCount, PracticeFrequency, EnsembleType, Genre } from '@shared/enums';
 
 @Schema()
+export class Location {
+  @Prop()
+  city: string;
+
+  @Prop() 
+  postCode: string;
+}
+
+@Schema()
 export class Ensemble extends Document {
   @Prop({ required: true })
   name: string;
 
   @Prop()
-  photo: string;
+  imageUrl: string;
 
   @Prop()
   description: string;
 
   @Prop()
-  homepage: string;
+  homepageUrl: string;
 
-  @Prop()
-  location: string;
+  @Prop({ type: Location})
+  location: Location;
 
   @Prop({ type: String, enum: MusicianCount, required: true })
   number_of_musicians: MusicianCount;
@@ -28,10 +37,10 @@ export class Ensemble extends Document {
   @Prop({ type: [String], enum: Genre, required: true })
   genres: Genre[];
 
-  @Prop({ type: [String], enum: EnsembleType })
+  @Prop({ type: [String], enum: EnsembleType, required: true })
   type: EnsembleType[];
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], required: true })
   member_ids: Types.ObjectId[];
 }
 
