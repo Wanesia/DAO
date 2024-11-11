8a230b25a2e67bdff5166b8927a09f6124c032fc
Wanesa Wintmiller <wintmiller161@gmail.com>
2024-11-11 23:43:39 +0100

    simple styling
    footer overlaps stuff but that is a task for later

--- apps/client/src/components/Login/LoginForm.tsx
+++ apps/client/src/components/Login/LoginForm.tsx
@@ -15,47 +15,60 @@ export default function LoginForm() {
   } = useForm<FormFields>();
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     try {
-        const response = await axios.post("http://localhost:3000/auth/login", data);
-        console.log("Logged in:", response.data);
+      const response = await axios.post(
+        "http://localhost:3000/auth/login",
+        data
+      );
+      console.log("Logged in:", response.data);
     } catch (error) {
-        setError("root",{message:"Invalid email or password"});
+      setError("root", { message: "Invalid email or password" });
     }
   };
   return (
-    <form className="form" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("email", {
-          required: "Email is required",
-          validate: (value) => {
-            if (!value.includes("@")) {
-              return "Email must include @";
-            }
-            return true;
-          },
-        })}
-        type="text"
-        placeholder="Email"
-      />
-      {errors.email && <div className="error">{errors.email.message}</div>}
-      <input
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters",
-          },
-        })}
-        type="password"
-        placeholder="Password"
-      />
-      {errors.password && (
-        <div className="error">{errors.password.message}</div>
-      )}
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <h1>Log ind</h1>
+        <label htmlFor="email">E-mail</label>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            validate: (value) => {
+              if (!value.includes("@")) {
+                return "Email must include @";
+              }
+              return true;
+            },
+          })}
+          type="text"
+          placeholder="E-mail"
+        />
+        {errors.email && (
+          <div className={styles.error}>{errors.email.message}</div>
+        )}
+        <label htmlFor="password">Adgangskode</label>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
+          type="password"
+          placeholder="Adgangskode"
+        />
+        {errors.password && (
+          <div className={styles.error}>{errors.password.message}</div>
+        )}
 
-      <button disabled={isSubmitting} type="submit">
-        {isSubmitting ? "Loading..." : "Log ind"}
-      </button>
-      {errors.root && <div className="error">{errors.root.message}</div>}
-    </form>
+        <button className={styles.btn} disabled={isSubmitting} type="submit">
+          {isSubmitting ? "Loading..." : "Log ind"}
+        </button>
+        {errors.root && (
+          <div className={styles.error}>{errors.root.message}</div>
+        )}
+        <p className={styles.reset}>Glemt adgangskode?</p>
+      </form>
+    </div>
   );
 }
