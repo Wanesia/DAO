4c2c12476c752a517468267708eb677355275794
Simona Tingleff Kardel <simonatingleffkardel@Simonas-MacBook-Pro.local>
2024-11-15 13:08:12 +0100

    add api directory

---
+++ apps/client/src/api/ensembleApi.ts
@@ -0,0 +1,12 @@
+// api/ensembleApi.ts
+import axiosInstance from './axiosInstance';
+
+/*export const createEnsemble = async (data: EnsembleData) => {
+  const response = await axiosInstance.post('/ensembles', data);
+  return response.data;
+};
+
+export const getEnsembles = async () => {
+  const response = await axiosInstance.get('/ensembles');
+  return response.data;
+};*/
