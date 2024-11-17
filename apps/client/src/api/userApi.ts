d7dc661c4472ed3c00a97cb096097bdefe6bb98b
Simona Tingleff Kardel <simonatingleffkardel@Simonas-MacBook-Pro.local>
2024-11-17 20:22:30 +0100

    add comments

--- apps/client/src/api/userApi.ts
+++ apps/client/src/api/userApi.ts
@@ -1,6 +1,7 @@
 import axiosInstance from "./axiosInstance";
 import { User } from "@shared/types";
 
+// don't know
 export const getUserStatus = async (): Promise<User> => {
   const response = await axiosInstance.get<User>("/auth/status");
   console.log(response.data);
