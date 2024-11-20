d81e82926386dc5f92e7c05f3fb93b5a7bd5107c
Wanesa Wintmiller <wintmiller161@gmail.com>
2024-11-20 01:17:22 +0100

    update axios interceptor

--- apps/client/src/api/axiosInstance.ts
+++ apps/client/src/api/axiosInstance.ts
@@ -7,4 +7,35 @@ const axiosInstance = axios.create({
   },
 });
 
+// if there is token the config has header authorization
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+      console.log("config with token");
+
+    }
+    console.log("config returned");
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor (for handling 401 errors)
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("401 Unauthorized - Redirecting");
+      window.location.href = "/unauthorized"; // Redirect to the unauthorized page
+    }
+    return Promise.reject(error);
+  }
+);
+
+
 export default axiosInstance;
