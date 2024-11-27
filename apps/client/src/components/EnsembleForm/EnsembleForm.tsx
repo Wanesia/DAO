85ddd717984bcf11c35d46aac461776fde403b7f
Simona Tingleff Kardel <simonatingleffkardel@Simonas-MacBook-Pro.local>
2024-11-27 08:49:08 +0100

    add constants to client, move dependencies

--- apps/client/src/components/EnsembleForm/EnsembleForm.tsx
+++ apps/client/src/components/EnsembleForm/EnsembleForm.tsx
@@ -12,39 +12,7 @@ import { createEnsemble } from "../../api/ensembleApi";
 import { useState } from "react";
 import { useNavigate } from "@tanstack/react-router";
 import DragAndDrop from "../DragAndDrop/DragAndDrop";
-
-
-// TODO: need to be moved to shared file again
-enum MusicianCount {
-  ONE_TO_FOUR = "1-4 musikere",
-  FIVE_TO_NINE = "5-9 musikere",
-  TEN_TO_TWENTY_FOUR = "10-24 musikere",
-  TWENTY_FIVE_TO_FORTY_NINE = "25-49 musikere",
-  FIFTY_PLUS = "Mere end 50 musikere",
-}
-
-enum PracticeFrequency {
-  MULTIPLE_TIMES_WEEK = "Flere gange om ugen",
-  ONCE_A_WEEK = "1 gang om ugen",
-  EVERY_OTHER_WEEK = "1 gang hver anden uge",
-  ONCE_A_MONTH = "1 gang om måneden",
-  EVERY_OTHER_MONTH = "1 gang hver anden måned eller",
-}
-
-enum EnsembleType {
-  CONTINUOUS = "Kontinuerligt",
-  PROJECT_BASED = "Projektbaseret",
-}
-
-enum Genre {
-  BAROK = "Barok",
-  FOLKEMUSIK = "Folkemusik",
-  KAMMERMUSIK = "Kammermusik",
-  ROMANTISK = "Romantisk",
-  SENMODERNE = "Senmoderne",
-  SENROMANTISK = "Senromantisk",
-  SYMFONISK = "Symfonisk",
-}
+import { MusicianCount, PracticeFrequency, EnsembleType, Genre } from "../../constants/enums";
 
 const EnsembleForm: React.FC = () => {
   const { control, handleSubmit, setError } = useForm<FieldValues>();
